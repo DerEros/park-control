b7531cc52079cebe70d3a8e07ee8520ddc69ad1e
DerEros <dereros@noreply.users.github.com>
2019-09-14 13:52:01 +0200

    Add logging library

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 BOARD=nodemcuv2
+LIBS=libraries/Arduino-Log
 
 include $(HOME)/git/makeEspArduino/makeEspArduino.mk
--- ParkControl.ino
+++ ParkControl.ino
@@ -1,5 +1,12 @@
+#include "libraries/Arduino-Log/ArduinoLog.h"
+
 void setup() {
+    Serial.begin(9600);
+    Log.begin(LOG_LEVEL_VERBOSE, &Serial);
+
+    Log.notice("Park Control - starting");
 }
 
 void loop() {
+    Log.verbose("Main loop starting");
 }
