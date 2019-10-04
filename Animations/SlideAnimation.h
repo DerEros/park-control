251806f81e58eff601861f1bbc0b76d852697de3
DerEros <dereros@noreply.users.github.com>
2019-10-04 22:07:59 +0200

    Move SlideAnimation to frame based animation

--- Animations/SlideAnimation.h
+++ Animations/SlideAnimation.h
@@ -1,42 +1,34 @@
 #ifndef __SLIDE_ANIMATION_H__
 #define  __SLIDE_ANIMATION_H__ 
 
-#include "AbstractAnimation.h"
+#include "AbstractFrameBasedAnimation.h"
 #include "IPixelPattern.h"
 
 template<typename TPixel>
-class SlideAnimation : public AbstractAnimation<TPixel> {
+class SlideAnimation : public AbstractFrameBasedAnimation<TPixel> {
     public:
         enum Direction {
             LEFT_TO_RIGHT,
             RIGHT_TO_LEFT
         };
 
-        SlideAnimation(PixelRange range, float frequency,
+        SlideAnimation(PixelRange range, unsigned int framesPerSecond,
                 IPixelPattern<TPixel> &pattern, Direction dir = LEFT_TO_RIGHT
                 ) :
-            AbstractAnimation<TPixel>(range),
+            AbstractFrameBasedAnimation<TPixel>(range, framesPerSecond),
             slidePattern(pattern),
             slideDirection(dir),
-            timeInFrame(0),
             patternOffset(0)
-        {
-            frameLengthMs = 1000.0 / frequency;
-        }
-
-        std::vector<TPixel> getPixels(unsigned int msSinceLastCall);
+        {}
 
     private:
         Direction slideDirection;
         const IPixelPattern<TPixel> &slidePattern;
 
-        float frameLengthMs;
-        unsigned int timeInFrame;
-
         unsigned int patternOffset;
 
-        void advanceAnimation(unsigned int msSinceLastCall);
-        void resetFrameTimer();
+        virtual void handleSwitchFrame();
+        virtual std::vector<TPixel> getFramePixels();
 
         void getPixelsLtr(std::vector<TPixel> &out);
         void getPixelsRtl(std::vector<TPixel> &out);
