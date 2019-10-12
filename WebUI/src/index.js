0f11284536f57ae25528efb4f337b3c6d0cca3db
DerEros <dereros@noreply.users.github.com>
2019-10-12 14:48:52 +0200

    Include materialize css into build

--- WebUI/src/index.js
+++ WebUI/src/index.js
@@ -1,8 +1,11 @@
+import 'materialize-css/dist/css/materialize.css';
+
 function createDocument() {
     const container = document.createElement('div');
 
     container.innerHTML = "<h1>Hello Park Control!</h1>";
     container.innerHTML += "Whazzup?";
+    container.innerHTML += "<a class='waves-effect waves-light btn'>My Button</a>";
 
     return container; 
 }
