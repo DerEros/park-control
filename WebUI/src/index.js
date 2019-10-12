4a06de12e71bb245649d62672c45dcbb4bef6074
DerEros <dereros@noreply.users.github.com>
2019-10-12 15:20:06 +0200

    Enable HTML fragments, Materialize JS components and icons

--- WebUI/src/index.js
+++ WebUI/src/index.js
@@ -1,4 +1,6 @@
-import 'materialize-css/dist/css/materialize.css';
+import 'materialize-css/dist/css/materialize.min.css';
+import './material-icons.css';
+import 'materialize-css/dist/js/materialize.min.js'
 
 function createDocument() {
     const container = document.createElement('div');
@@ -6,8 +8,16 @@ function createDocument() {
     container.innerHTML = "<h1>Hello Park Control!</h1>";
     container.innerHTML += "Whazzup?";
     container.innerHTML += "<a class='waves-effect waves-light btn'>My Button</a>";
+    container.innerHTML += "<br/>";
+    container.innerHTML += require('./greeting.html');
+    container.innerHTML += require('./collapsible-test.html');
 
     return container; 
 }
 
 document.body.appendChild(createDocument());
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, {});
+});
