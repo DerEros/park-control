cc05a5c324c120f76f188f08095f2a46464f37b8
DerEros <dereros@noreply.users.github.com>
2019-10-12 14:37:58 +0200

    Implement dev-server mode, add materialize css

--- WebUI/src/index.js
+++ WebUI/src/index.js
@@ -2,6 +2,7 @@ function createDocument() {
     const container = document.createElement('div');
 
     container.innerHTML = "<h1>Hello Park Control!</h1>";
+    container.innerHTML += "Whazzup?";
 
     return container; 
 }
