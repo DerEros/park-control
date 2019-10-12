afc98ab60355650f326d5e7699a478473c304029
DerEros <dereros@noreply.users.github.com>
2019-10-12 14:28:09 +0200

    Move to auto-generated HTML

--- WebUI/src/index.js
+++ WebUI/src/index.js
@@ -1,1 +1,9 @@
-alert("Hello Park Control!");
+function createDocument() {
+    const container = document.createElement('div');
+
+    container.innerHTML = "<h1>Hello Park Control!</h1>";
+
+    return container; 
+}
+
+document.body.appendChild(createDocument());
