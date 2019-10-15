a5e438d33a52620476537854b42e9b09af9e18a5
DerEros <dereros@noreply.users.github.com>
2019-10-15 16:16:30 +0200

    Attempt to use PEAP based WiFi authentication
    Not successful yet

--- App/Network.cpp
+++ App/Network.cpp
@@ -2,11 +2,13 @@
 
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
+#include <wpa2_enterprise.h>
 
 #include "../libraries/Arduino-Log/ArduinoLog.h"
 
-const char* ssid = "SSID";
-const char* pwd = "PWD";
+const char* ssid = "ssid";
+const char* username = "user";
+const char* pwd = "pwd";
 const char* dnsName = "ParkControl";
 
 void Network::start() {
@@ -14,10 +16,28 @@ void Network::start() {
 
     Serial.setDebugOutput(true);
 
+
+    wifi_set_opmode(STATION_MODE);
+    struct station_config stationConf;
+    memset(&stationConf, 0, sizeof(stationConf));
+    strlcpy((char*)&stationConf.ssid, ssid, 32);
+    strlcpy((char*)&stationConf.password, pwd, 64);
+    wifi_station_set_config(&stationConf);
+
+    wifi_station_clear_cert_key();
+    wifi_station_clear_enterprise_ca_cert();
+
+    wifi_station_set_wpa2_enterprise_auth(1);
+    wifi_station_set_enterprise_identity((unsigned char*)username, strlen(username));
+    wifi_station_set_enterprise_username((unsigned char*)username, strlen(username));
+    wifi_station_set_enterprise_password((unsigned char*)pwd, strlen(pwd));
+    wifi_station_connect();
+    
     Log.notice("... \n");
-    WiFi.printDiag(Serial);
+    //WiFi.printDiag(Serial);
     Log.notice("... \n");
-    WiFi.begin(ssid, pwd);
+
+    //WiFi.begin(ssid, pwd);
     Log.trace("Attempting connection to WiFi ");
 
     while (WiFi.status() != WL_CONNECTED) {
